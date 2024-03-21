1d44f8d443da475e6a925716606d221d945b949c
Swakshan <tsswakshan@gmail.com>
2024-03-21 16:47:42 +0530

    feat(Twitter): Added Ad block hooks

--- app/src/main/java/app/revanced/integrations/twitter/settings/SettingsStatus.java
+++ app/src/main/java/app/revanced/integrations/twitter/settings/SettingsStatus.java
@@ -12,12 +12,19 @@ public class SettingsStatus {
     public static boolean hideBanner = false;
     public static boolean hideForyou = false;
 
+    public static boolean hideAds = false;
+    public static boolean hideGAds = false;
+    public static boolean hideWTF = false;
+    public static boolean hideCTS = false;
+    public static boolean hideCTJ = false;
+    public static boolean hideDetailedPosts = false;
+    public static boolean hideRBMK = false;
+
 
     public static void enableDownloadFolder() { changeDownloadEnabled = true; }
     public static void enableFont() { enableFontMod = true; }
 
     public static void hideRecommendedUsers() { hideRecommendedUsers = true; }
-    public static void hidePromotedTrends() { hidePromotedTrend = true; }
     public static void hideCommunityNotes() { hideCommunityNote = true; }
     public static void hideFAB() { hideFAB = true; }
     public static void hideViewCount() { hideViewCount = true; }
@@ -25,9 +32,18 @@ public class SettingsStatus {
     public static void hideBanner() { hideBanner = true; }
     public static void hideForYou() { hideForyou = true; }
 
+    public static void hideAds() { hideAds = true; }
+    public static void hideGAds() { hideGAds = true; }
+    public static void hideWhoToFollow() { hideWTF = true; }
+    public static void hideCreatorsToSub() { hideCTS = true; }
+    public static void hideCommToJoin() { hideCTJ = true; }
+    public static void hideDetailedPost() { hideDetailedPosts = true; }
+    public static void hideRevistBookmark() { hideRBMK = true; }
+    public static void hidePromotedTrends() { hidePromotedTrend = true; }
+
     public static boolean enableTimelineSection(){ return (hideLiveThreads || hideBanner || hideForyou); }
     public static boolean enableMiscSection() { return (enableFontMod || hideRecommendedUsers || hideFAB || hideViewCount || hideCommunityNote); }
-    public static boolean enableAdsSection() {return (hidePromotedTrend); }
+    public static boolean enableAdsSection() {return (hideAds|| hideGAds || hideWTF || hideCTS || hideCTJ || hideDetailedPosts || hideRBMK ||hidePromotedTrend); }
 
     public static void load() {}
 }
