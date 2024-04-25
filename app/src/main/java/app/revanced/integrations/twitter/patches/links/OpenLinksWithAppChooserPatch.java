ed95b79bc8d87b9d08d9ca6b99d990efa1c8a468
dic1911 <d69873@gmail.com>
2024-04-25 17:50:53 +0800

    fix: don't use chooser for internal stuff

--- app/src/main/java/app/revanced/integrations/twitter/patches/links/OpenLinksWithAppChooserPatch.java
+++ app/src/main/java/app/revanced/integrations/twitter/patches/links/OpenLinksWithAppChooserPatch.java
@@ -2,9 +2,21 @@ package app.revanced.integrations.twitter.patches.links;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Objects;
+import java.util.Set;
 
 public final class OpenLinksWithAppChooserPatch {
-    public static void openWithChooser(final Context context, final Intent originalIntent) {
+    public static void openWithChooser(final Context context, final Intent originalIntent, final Bundle bundle) {
+        Set<String> categories = originalIntent.getCategories();
+
+        // original credit: TwiFucker, updated for newer build
+        if (originalIntent.getAction() == null) {
+            context.startActivity(originalIntent, bundle);
+            return;
+        }
+
         final Intent intent = new Intent("android.intent.action.VIEW", originalIntent.getData());
         context.startActivity(Intent.createChooser(intent, null));
     }
